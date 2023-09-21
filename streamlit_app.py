503ec1a47340009c58439354f52fcab09b5d5d35
m3ayys <144980844+m3ayys@users.noreply.github.com>
2023-09-21 04:31:53 +0000

    aaaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import random
 st.title ramdom
-if st. button("おみくじをひく"）
-results=("大吉","中吉","小吉","吉","凶","大凶")
-result=ramdom.choice(results)
-st.white(f"結果:(result)")
+if st. button("おみくじをひく"）:
+    results=("大吉","中吉","小吉","吉","凶","大凶")
+    result=ramdom.choice(results)
+    st.white(f"結果:(result)")
