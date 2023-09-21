5ba57b6ec67f890aa2c4f28b636ee3497528c73a
m3ayys <144980844+m3ayys@users.noreply.github.com>
2023-09-21 04:36:45 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import random
 st.title("おみくじアプリ")
 if st. button("おみくじをひく"):
     results=("大吉","中吉","小吉","吉","凶","大凶")
-    result=ramdom.choice(results)
+    result = ramdom.choice(results)
     st.write(f"結果:(result)")
